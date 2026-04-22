710440b19956ece8702bf012766ebee002fea123
oussema karia <bro188411@gmail.com>
2026-04-22 15:14:14 +0100

    analyseur fixed

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
 import { SpeedInsights } from "@vercel/speed-insights/react";
-
+import { Analytics } from "@vercel/analytics/react"
 import App from "./App.tsx";
 import "./index.css";
 import { AppProvider } from "./context/AppContext";
